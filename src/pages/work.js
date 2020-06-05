173a9dc9b65af9acb7f8506bb3e291f20596e7b9
Rabimba Karanjai <rabimba@mozilla.com>
2020-06-05 21:56:27 +0530

    Changed title

--- src/pages/work.js
+++ src/pages/work.js
@@ -13,8 +13,8 @@ const WorkTitle = styled("h1")`
 const Work = ({ projects, meta }) => (
     <>
         <Helmet
-            title={`Work | Prist, Gatsby & Prismic Starter`}
-            titleTemplate={`%s | Work | Prist, Gatsby & Prismic Starter`}
+            title={`Work | Rabimba's Rambling`}
+            titleTemplate={`%s | Work | Rabimba's Rambling`}
             meta={[
                 {
                     name: `description`,
@@ -22,7 +22,7 @@ const Work = ({ projects, meta }) => (
                 },
                 {
                     property: `og:title`,
-                    content: `Work | Prist, Gatsby & Prismic Starter`,
+                    content: `Work | Rabimba's Rambling`,
                 },
                 {
                     property: `og:description`,
